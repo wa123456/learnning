633596412b27ced8a419da27c8014cf0f392b945
unknown <2206794631@qq.com>
2019-06-11 23:30:18 +0800

    'reflect'

--- src/com/lv/reflect/ClassDemo1.java
+++ src/com/lv/reflect/ClassDemo1.java
@@ -7,8 +7,31 @@ public class ClassDemo1 {
         Class c1 = Foo.class;
         Class c2 = foo.getClass();
         System.out.println(c1 == c2);
+
+        Class c3 = null;
+        try {
+            c3 = Class.forName("com.lv.reflect.Foo");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(c2 == c3);
+        Foo foo2 = null;
+        try {
+            foo2 = (Foo)c1.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        foo2.print();
     }
 }
 
-class Foo{}
+class Foo{
+    void print(){
+        System.out.println("Foo!!!!");
+    }
+
+}
 
